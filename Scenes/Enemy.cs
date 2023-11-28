f9ef99816d93a873a990d75196a052af0eedf97f
Tekkila <gordogamer8@gmail.com>
2023-11-28 20:32:19 -0300

    Enemy Movement

---
+++ Scenes/Enemy.cs
@@ -0,0 +1,76 @@
+using Godot;
+using System;
+
+public partial class Enemy : CharacterBody2D
+{
+    player player;
+    [Export] float enemySpeed = 250f;
+    [Export] float damage = 10f;
+    [Export] float aps = 2f;
+    
+    float attack_speed;
+    float time_until_attack;
+    bool within_attack_range = false;
+
+    public override void _Ready()
+    {
+        player = (player)GetTree().Root.GetNode("World").GetNode("Player");
+        GD.Print(player.Name);
+        attack_speed = 1 / aps;
+        time_until_attack = attack_speed;
+    }
+
+    public override void _Process(double delta)
+    {
+        if (within_attack_range && time_until_attack <= 0)
+        {
+            Attack();
+            time_until_attack = attack_speed;
+        }
+        else
+        {
+            time_until_attack -= (float)delta;
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if (player != null)
+        {
+            LookAt(player.GlobalPosition);
+            Vector2 direction = (player.GlobalPosition - GlobalPosition).Normalized();
+            Velocity = direction * enemySpeed;
+        }
+        else
+        {
+            Velocity = Vector2.Zero;
+        }
+
+        MoveAndSlide();
+    }
+
+    public void Attack()
+    {
+        GD.Print("Attack player");
+    }
+
+    public void OnAttackRangeBodyEnter(Node2D body)
+    {
+        if (body.IsInGroup("player"))
+        {
+            GD.Print("entered");
+            within_attack_range = true;
+        }
+    }
+
+    public void OnAttackRangeBodyExit(Node2D body)
+    {
+        if (body.IsInGroup("player"))
+        {
+            GD.Print("Exit");
+            within_attack_range = true;
+            time_until_attack = attack_speed;
+        }
+    }
+
+}
--- Scenes/Gun.cs
+++ Scenes/Gun.cs
@@ -7,6 +7,7 @@ public partial class Gun : Node2D
     [Export] float bullet_speed = 600f;
     [Export] float bps = 5;
     [Export] float bullet_damage = 30f;
+    [Export] int gun_ammo = 6;
 
     float fire_rate;
 
@@ -19,7 +20,7 @@ public partial class Gun : Node2D
 
     public override void _Process(double delta)
     {
-        if (Input.IsActionJustPressed("click") && time_until_fire > fire_rate)
+        if (Input.IsActionJustPressed("click") && time_until_fire > fire_rate && gun_ammo > 0)
         {
             RigidBody2D bullet = bullet_scn.Instantiate<RigidBody2D>();
             bullet.Rotation = GlobalRotation;
@@ -27,7 +28,7 @@ public partial class Gun : Node2D
             bullet.LinearVelocity = bullet.Transform.X * bullet_speed;
             
             GetTree().Root.AddChild(bullet);
-
+            gun_ammo -= 1;
             time_until_fire = 0f;
         } else {
             time_until_fire += (float)delta;
